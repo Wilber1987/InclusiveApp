b69b00d1a4b48d8b47cb7e7ddd4acb8d5c99db8c
Wilber1987 <wilberj1987@gmail.com>
2020-10-14 17:16:18 -0600

    30-10

--- www/Scripts/modules/WComponentsTools.js
+++ www/Scripts/modules/WComponentsTools.js
@@ -1,106 +1,103 @@
 function DisplayAcordeon(value, SectionId, size = null) {
-    console.log(value)
-    if (size == null) {
-      size = "500px";
+  console.log(value)
+  if (size == null) {
+    size = "500px";
+  }
+  var acc = document.getElementsByClassName("GroupFormAcordeon");
+  for (var i = 0; i < acc.length; i++) {
+    ventanaM = acc[i];
+    if (ventanaM.id != SectionId) {
+      //  if (ventanaM.style.height != "300px") {
+      ventanaM.style.transition = "all ease 1s";
+      ventanaM.style.height = "0px";
+      //ventanaM.style.overflow = "scroll";
+    } else {
+      ventanaM.style.transition = "all ease 1s";
+      ventanaM.style.height = size;
+      //ventanaM.style.oveflow = "hidden";
     }
-    // var ventanaM = document.getElementById(idModal);
-    var acc = document.getElementsByClassName("GroupFormAcordeon");
-    for (var i = 0; i < acc.length; i++) {
-      ventanaM = acc[i];  
-      if (ventanaM.id != SectionId) {
-        //  if (ventanaM.style.height != "300px") {
-        ventanaM.style.transition = "all ease 1s";
-        ventanaM.style.height = "0px";
-        //ventanaM.style.overflow = "scroll";
-      } else {
-        ventanaM.style.transition = "all ease 1s";
-        ventanaM.style.height = size;
-        //ventanaM.style.oveflow = "hidden";
+  }
+}
+function DisplayUniqAcorden(elementId) {
+  let SectionElement = GetObj(elementId);
+  if (SectionElement.style.display == "none") {
+    SectionElement.style.display = "block";
+    setTimeout(() => {
+      SectionElement.style.maxHeight = "800px";
+      SectionElement.style.minHeight = "300px";
+    }, 100);
+  } else {
+    SectionElement.style.maxHeight = "0px";
+    SectionElement.style.minHeight = "0px";
+    setTimeout(() => {
+      SectionElement.style.display = "none";
+    }, 1000);
+  }
+}
+
+//*****DOM TOOLS */
+
+//AJAXTOOLS----------------------------------------
+
+class AjaxTools {
+  constructor() {
+  }
+  static PostRequest = async (Url, Data = {}) => {
+    try {
+      let response = await fetch(Url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+        body: JSON.stringify(Data)
+      });
+      if (response.status == 404 || response.status == 500) {
+        console.log("ocurrio un error: " + response.status);
+        if (typeof responseLocal !== "undefined" && typeof responseLocal !== "null" && responseLocal != "") {
+          return this.LocalData(Url);
+        } else {
+          return [];
+        }
+      }
+      else {
+        response = await response.json();
+        localStorage.setItem(Url, JSON.stringify(response));
+        console.log(response);
+        return response;
+      }
+    } catch (error) {
+      if (error == "TypeError: Failed to fetch") {
+        return this.LocalData(Url);
       }
     }
   }
-  function DisplayUniqAcorden(elementId){
-      let SectionElement = GetObj(elementId);       
-      if (SectionElement.style.display == "none") {
-          SectionElement.style.display = "block";         
-          setTimeout(()=>{               
-              SectionElement.style.maxHeight = "800px"; 
-              SectionElement.style.minHeight = "300px";              
-          }, 100);                            
-      }else {
-          SectionElement.style.maxHeight = "0px";  
-          SectionElement.style.minHeight = "0px";        
-          setTimeout(()=>{            
-              SectionElement.style.display = "none";             
-          }, 1000); 
+  static GetRequest = async (Url) => {
+    try {
+      let response = await fetch(Url, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
+      });
+      if (response.status == 404 || response.status == 500) {
+        console.log("ocurrio un error: " + response.status);
+        if (typeof responseLocal !== "undefined" && typeof responseLocal !== "null" && responseLocal != "") {
+          return this.LocalData(Url);
+        } else {
+          return [];
+        }
       }
+      else {
+        response = await response.json();
+        localStorage.setItem(Url, JSON.stringify(response));
+        return response;
+      }
+    } catch (error) {
+      if (error == "TypeError: Failed to fetch") {
+        return this.LocalData(Url);
+      }
+    }
   }
-
-  //*****DOM TOOLS */
-
-  //AJAXTOOLS----------------------------------------
-
-  class AjaxTools {
-    constructor(){
-    }
-    static PostRequest =  async (Url, Data = {})=>{ 
-      try {
-        let response = await fetch(Url , {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-            body: JSON.stringify(Data)
-        });     
-        if (response.status == 404 || response.status ==  500) {
-          console.log("ocurrio un error: " + response.status);      
-          if (typeof responseLocal !== "undefined" && typeof responseLocal !== "null" && responseLocal != "") {
-            return this.LocalData(Url); 
-          }else{
-            return [];
-          }  
-        }
-        else {
-          response = await response.json();
-          localStorage.setItem(Url, JSON.stringify(response));
-          console.log(response);
-          return response;
-        }    
-      } catch (error) { 
-        if (error == "TypeError: Failed to fetch") {
-          return this.LocalData(Url);
-        } 
-      }    
-    }
-    
-    static GetRequest =  async (Url)=>{  
-      try {
-        let response = await fetch(Url , {
-          method: 'GET',
-          headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' }
-        });     
-        if (response.status == 404 || response.status ==  500) {
-          console.log("ocurrio un error: " + response.status);    
-          if (typeof responseLocal !== "undefined" && typeof responseLocal !== "null" && responseLocal != "") {
-            return this.LocalData(Url); 
-          }else{
-            return [];
-          }  
-        }
-        else {
-          response = await response.json();
-          localStorage.setItem(Url, JSON.stringify(response));
-          return response;
-        }    
-      } catch (error) {
-        if (error == "TypeError: Failed to fetch") {
-          return this.LocalData(Url);
-        } 
-      }  
-    }
-    LocalData = (Url)=>{
-      let responseLocal = localStorage.getItem(Url);
-      return JSON.parse(responseLocal);  
-    }
-
+  LocalData = (Url) => {
+    let responseLocal = localStorage.getItem(Url);
+    return JSON.parse(responseLocal);
   }
+}
 
 
